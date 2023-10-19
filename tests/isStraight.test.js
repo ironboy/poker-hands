d96d9ab178b3fd6059c6fecb06bd0e8a0545578a
Ironboy <ironboy@users.noreply.github.com>
2023-10-19 13:50:07 +0200

    changes in tests

--- tests/isStraight.test.js
+++ tests/isStraight.test.js
@@ -4,5 +4,5 @@ const suits = '♥♦♣♠';
 
 test('check that isStraight returns truthy if straight', () => {
   let hand = new Hand('♥9', '♦8', '♣7', '♥5', '♦6');
-  expect(CompareHands.isStraight(hand)).toBeTruthy();
+  expect(CompareHands.isStraight(hand)).toBeFalsey();
 });
